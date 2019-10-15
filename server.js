76e7d351585d992282d9ebbfc840d5178096c4cf
vonbarown <vonielbrown@pursuit.org>
2019-10-14 21:28:13 -0400

    initializing the server

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+const handleCors = (req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*')
+    next()
+}
+
+const port = 8080;
+app.use(handleCors)
+
+const isAnimal = (req, res, next) => {
+    let input = req.params.input
+    const animals = ["jackal", "whale", "worm", "stork", "frog", "fox", "guppy"]
+
+    animals.includes(input) ? res.json({
+        status: "sucess,you entered an animal",
+        message: true
+    }) : next()
+}
+
+const errorMessage = (req, res, next) => {
+    res.json({
+        status: "Are you sure this is an animal ?",
+        message: false
+    })
+}
+
+app.get('/animal/:input', isAnimal, errorMessage)
+
+
+const getRandomNum = (req, res, next) => {
+    let floor = req.query.floor;
+    let ceil = req.query.max
+    return Math.floor((Math.random() * max) + floor)
+};
+
+app.get('/random')
+
+app.listen(port, () => {
+    console.log(`Running at http://localhost:${port}/`);
+})
