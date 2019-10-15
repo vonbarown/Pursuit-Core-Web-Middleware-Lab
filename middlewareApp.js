30015c38bf346887812e7db9fabae5310ea713b4
vonbarown <vonielbrown@pursuit.org>
2019-10-15 19:37:42 -0400

    adding all of the button and functions

--- middlewareApp.js
+++ middlewareApp.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let randomButton = document.querySelector('#randomButton')
     randomButton.addEventListener('click', getRandomNum)
+
+    let peek = document.querySelector('#peek')
+    peek.addEventListener('click', getRandomNum)
 })
 
 
@@ -40,27 +43,60 @@ const getRandomNum = async () => {
     console.log(data);
 
     clearScreen()
-    let searchResults = document.querySelector('.searchResults')
+    let randomResults = document.querySelector('.randomResults')
     let status = document.createElement('p');
-    let message = document.createElement('p');
+    let range = document.createElement('p');
+    let randNum = document.createElement('p');
 
     status.innerText = `Status: ${data.status}`;
-    message.innerText = `Message: ${data.message}`;
+    range.innerText = `Your range is: ${data.range}`;
+    randNum.innerText = `The random number is: ${data.randPick}`;
 
-    searchResults.append(status, message)
+    randomResults.append(status, range, randPick)
 }
 
-// const displayToScreen = (data) => {
-//     clearScreen()
-//     let searchResults = document.querySelector('.searchResults')
-//     let status = document.createElement('p');
-//     let message = document.createElement('p');
+const peek = async () => {
 
-//     status.innerText = `Status: ${data.status}`;
-//     message.innerText = `Message: ${data.message}`;
+    let queueButton = document.querySelector('#queueInput').value
+    let name = document.querySelector('#inputName').value
+    let enqueueUrl = `http://localhost:8080/queue/peek`
 
-//     searchResults.append(status, message)
-// }
+    const {
+        data
+    } = await axios.get(enqueueUrl);
+    clearScreen()
+
+    let results = document.querySelector('.results')
+    let status = document.createElement('p');
+    let peekData = document.createElement('p');
+
+
+    status.innerText = `Status: ${data.status}`;
+    peekData.innerText = `Your range is: ${data.data}`;
+
+    results.append(status, peekData)
+}
+
+const enqueue = async () => {
+
+    let name = document.querySelector('#inputName').value
+    let enqueueUrl = `http://localhost:8080/queue/ls?name=${name}`
+
+    const {
+        data
+    } = await axios.get(enqueueUrl);
+    clearScreen()
+
+    let results = document.querySelector('.results')
+    let status = document.createElement('p');
+    let enqueued = document.createElement('p');
+
+
+    status.innerText = `Status: ${data.status}`;
+    enqueued.innerText = `Your range is: ${data.enqueued}`;
+
+    results.append(status, enqueued)
+}
 
 const clearScreen = () => {
     let container = document.querySelector('.searchResults');
